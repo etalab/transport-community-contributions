e0596a29da69025bf4bb3964b7ab8b90f9385419
Francis <fchabouis@gmail.com>
2020-12-29 12:02:59 +0100

    reload page for a new test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -11,11 +11,15 @@ describe('valid file upload', () => {
     cy.get('[data-cy="is-file-valid"]').contains('Le fichier est valide')
     cy.get('[data-cy="request-modification-form"]').contains('Soumettre la demande de modification')
     cy.get('[data-cy="submit-button"]').contains('Envoyer')
+
+    
+
   })
 })
 
 describe('upload invalid file', () => {
   it('Download the file', () => {
+    cy.reload()
     const invalid_db_fixture = 'invalid-base.csv';
     cy.get('[data-cy="file-input"]').attachFile(invalid_db_fixture);
     cy.get('[data-cy="is-file-valid"]').contains('Le fichier n\'est pas valide')
