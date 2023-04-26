64d8f434744594b8be370355fd266e31c2cb0f15
Francis Chabouis <fchabouis@gmail.com>
2023-04-26 15:28:23 +0200

    first step

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,20 @@ import { createVuePlugin as vue } from "vite-plugin-vue2";
 // https://vitejs.dev/config/
 const path = require("path");
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        compatConfig: {
+          MODE: 2
+        }
+      }
+    }
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      'node-fetch': 'isomorphic-fetch'
+      'node-fetch': 'isomorphic-fetch',
+      'vue': '@vue/compat'
     },
   },
 });
